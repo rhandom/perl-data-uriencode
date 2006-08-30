7975a821823e957a56dd153f67551a1f3b4e64ea
paul <paul>
2006-08-30 17:27:50 +0000

    rework to work better

--- lib/Data/URIEncode.pm
+++ lib/Data/URIEncode.pm
@@ -14,7 +14,7 @@ use vars qw($MAX_ARRAY_EXPAND
 
 BEGIN {
     $MAX_ARRAY_EXPAND = 100;
-    $qr_chunk = '[^.:]*';
+    $qr_chunk = '([^.:]*)';
     $qr_chunk_quoted = '"((?:[^"]*|\\\\")+)(?<!\\\\)(")';
 }
 
@@ -24,73 +24,79 @@ sub flat_to_complex {
     my $in = shift || die "Missing hashref";
 
     my $out = {};
+
     foreach my $key (sort keys %$in) {
-        my $val = $in->{$key};
-
-        ### non-complex quoted keys
-        if ($key =~ /^ $qr_chunk_quoted $/xo) {
-            $key = $1;
-            $key =~ s/\\\"/\"/g;
-            $out->{$key} = $val;
-            next;
-
-        ### normal keys
-        } elsif ($key !~ /[.:]/) {
-            $out->{$key} = $val;
-            next;
-        }
-
-        # looks like foo.0.bar which would map to foo => [{bar => $val}]
-        my $copy = $key;
+        my $copy = ($key =~ /^[.:]/) ? $key : ".$key";
         my $ref  = $out;
-        $copy =~ s/^ $qr_chunk_quoted //sxo || $copy =~ s/^ ($qr_chunk) //xo;
-        my $name = $1;
-        $name =~ s/\\\"/\"/g if $2;
+        my $name = 'root';
 
-        while (   $copy =~ s/^ ([.:]) $qr_chunk_quoted//xo
-               || $copy =~ s/^ ([.:]) ($qr_chunk)//xo) {
+        while ($copy =~ s/^ ([.:]) $qr_chunk_quoted//xo
+               || $copy =~ s/^ ([.:]) $qr_chunk//xo) {
             my ($sep, $next) = ($1, $2);
-            $name =~ s/\\\"/\"/g if $3;
-            if (ref $ref eq 'HASH') {
-                if (! exists $ref->{$name}) {
-                    if ($next =~ /^\d+$/ && $sep ne ':') {
-                        $ref->{$name} = [];
-                    } else {
-                        $ref->{$name} = {};
-                    }
+            $next =~ s/\\\"/\"/g if $3;
+
+            if (ref $ref eq 'ARRAY') {
+                if (! exists $ref->[$name]) {
+                    $ref->[$name] = $sep eq ':' ? [] : {};
+                } else {
+                    die "Can't coerce array into hash near \"$name\" while unfolding $key"
+                        if $sep ne ':';
                 }
-                $ref = $ref->{$name};
-                $name = $next;
-            } elsif (ref $ref eq 'ARRAY') {
                 die "Can't use $name as index value for an array while unfolding $key"
                     if $name !~ /^\d+$/;
                 die "Can't expand array in $key by more than $MAX_ARRAY_EXPAND"
                     if $name - $#$ref > $MAX_ARRAY_EXPAND;
-                if (! exists $ref->[$name]) {
-                    if ($next =~ /^\d+$/ && $sep ne ':') {
-                        $ref->[$name] = [];
-                    } else {
-                        $ref->[$name] = {};
-                    }
+                $ref  = $ref->[$name];
+                $name = $next;
+            } elsif (ref $ref eq 'HASH') {
+                if (! exists $ref->{$name}) {
+                    $ref->{$name} = $sep eq ':' ? [] : {};
                 }
-                $ref = $ref->[$name];
+                $ref  = $ref->{$name};
                 $name = $next;
             } else {
-                die "Can't unfold $key at level $name (scalar value exists)";
+                die "Unknown type during unfold of $key";
+            }
+
+            if ($sep eq ':') {
+                die "Can't coerce hash into array near \"$name\" while unfolding $key"
+                    if ref $ref eq 'HASH';
+            } else {
+                die "Can't coerce array into hash near \"$name\" while unfolding $key"
+                    if ref $ref eq 'ARRAY';
             }
         }
+
+
         if (ref $ref eq 'HASH') {
-            $ref->{$name} = $val;
+            $ref->{$name} = $in->{$key};
         } elsif (ref $ref eq 'ARRAY') {
             die "Can't use $name as index value for an array while unfolding $key"
                 if $name !~ /^\d+$/;
             die "Can't expand array in $key by more than $MAX_ARRAY_EXPAND"
                 if $name - $#$ref > $MAX_ARRAY_EXPAND;
-            $ref->[$name] = $val;
+            $ref->[$name] = $in->{$key};
         } else {
             die "Can't unfold $key at level $name (scalar value exists)";
         }
     }
+
+    return $out->{'root'};
+}
+
+###----------------------------------------------------------------###
+
+sub complex_to_flat {
+    my $in     = shift;
+    my $out    = shift || {};
+    my $prefix = shift;
+    my $is_top;
+    if (! defined $prefix || $prefix eq '') {
+        $prefix = '';
+        $is_top = 1;
+    }
+
+
     return $out;
 }
 
